63f1a1bd6270ce291894008c4296b3db3157179a
Kevin Davis <contact@kevind.co.uk>
2023-05-03 19:50:34 +0200

    Changed the menu on mobile to slide down with an animation using css grid - which replaces previous method of sliding menu in (as on close there was no animation)

--- scripts.js
+++ scripts.js
@@ -3,40 +3,34 @@ const menu = document.querySelectorAll(".menu-headers-head")
 let windowWidth = window.innerWidth
 
 if (window.innerWidth < 900) {
-    document.getElementById("meat").parentElement.classList.remove("menu-dark")
+    document.getElementById("meat").parentElement.parentElement.classList.remove("menu-dark")
 }
 
 function displayItems(event) {
     const id = event.target.dataset.heading
     const menuToOpen = document.getElementById(id)
-    const chevron = document.getElementById(id).previousElementSibling.querySelector("i")
+    const chevron = document.getElementById(id).parentElement.previousElementSibling.querySelector("i")
 
-    if (!menuToOpen.classList.contains("open") && window.innerWidth <= 900) {
-        menuToOpen.classList.add("open")
+    if (!menuToOpen.parentElement.classList.contains("open") && window.innerWidth <= 900) {
+        menuToOpen.parentElement.classList.add("open")
         chevron.style.transform = "rotate(90deg)"
         chevron.style.color = "var(--color-persian-orange)"
-        menuToOpen.previousElementSibling.querySelector("h4").style.color = "var(--color-persian-orange)"
+        menuToOpen.parentElement.previousElementSibling.querySelector("h4").style.color = "var(--color-persian-orange)"
 
     } else {
-        menuToOpen.classList.remove("open")
+        menuToOpen.parentElement.classList.remove("open")
         chevron.style.transform = "rotate(0)"
         chevron.style.color = "inherit"
-        menuToOpen.previousElementSibling.querySelector("h4").style.color = "inherit"
-    }
-
-    if (id === "meat" && menuToOpen.classList.contains("open") && window.innerWidth <= 900) {
-        menuToOpen.parentElement.classList.add("menu-dark")
-    } else if (id === "meat" && window.innerWidth <= 900) {
-        document.getElementById("meat").parentElement.classList.remove("menu-dark")
+        menuToOpen.parentElement.previousElementSibling.querySelector("h4").style.color = "inherit"
     }
 }
 
 function screenChange() {
     // console.log(window.innerWidth)
     if (window.innerWidth <= 900 && !document.getElementById("meat").classList.contains("open")) {
-        document.getElementById("meat").parentElement.classList.remove("menu-dark")
+        document.getElementById("meat").parentElement.parentElement.classList.remove("menu-dark")
     } else {
-        document.getElementById("meat").parentElement.classList.add("menu-dark")
+        document.getElementById("meat").parentElement.parentElement.classList.add("menu-dark")
     }
 }
 
